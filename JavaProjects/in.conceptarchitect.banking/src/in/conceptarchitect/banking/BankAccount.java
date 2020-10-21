9ea1a44ff42b2f51315d3ea83e73c356149264f0
Vivek Dutta Mishra <vivek@conceptarchitect.in>
2020-10-21 16:01:51 +0530

    day#3-update#3

--- JavaProjects/in.conceptarchitect.banking/src/in/conceptarchitect/banking/BankAccount.java
+++ JavaProjects/in.conceptarchitect.banking/src/in/conceptarchitect/banking/BankAccount.java
@@ -1,5 +1,7 @@
 package in.conceptarchitect.banking;
 
+import in.conceptarchitect.utils.Input;
+
 public class BankAccount {
 	
 	int accountNumber;
@@ -8,11 +10,46 @@ public class BankAccount {
 	double balance;
 	double interestRate;
 	
+	
+	public int getAccountNumber() {
+		return accountNumber;
+	}
+	
+	//you can't change your account number
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name=name;
+	}
+	
+	//balance
+	public double getBalance() {
+		return balance;
+	}
+	
+	//no set balance. to set balance use deposit or withdraw
+	
+	//get set interest rate
+	public double getInterestRate() {
+		return interestRate;
+	}
+
+	public void setInterestRate(double interestRate) {
+		this.interestRate = interestRate;
+	}
+
+	
+	
+	
 	public  BankAccount(int accountNumber, String name, String password, double amount, double rate) {
 		createAccount(accountNumber, name, password,amount, rate);
 	}
 	
-	public void createAccount(int accountNumber, String name, String password, double amount, double rate) {
+	
+	//I don't want anyone else calling this function
+	private void createAccount(int accountNumber, String name, String password, double amount, double rate) {
 
 		this.accountNumber=accountNumber;
 		this.name=name;  //this.name is class field, name is argument
@@ -32,6 +69,49 @@ public class BankAccount {
 	
 	public void deposit() {
 		//TODO: write the deposit logic
+		Input input=new Input();
+		
+		int amount=input.readInt("amount to deposit?");
+		if(amount>0) {
+			balance+=amount;
+			System.out.println("Amount deposited successfully");
+		}
+		else {
+			System.out.println("Invalid Amount. Deposit Failed");
+		}
+		
+	}
+
+	public void withdraw() {
+		// TODO Auto-generated method stub
+		Input input=new Input();
+		
+		int amount=input.readInt("amount to withdraw?");
+		String password=input.readString("password");
+		
+		if(!this.password.equals(password)) {
+			System.out.println("invalid credentials");
+			return ;
+		}else if(amount<=0) {
+			
+			System.out.println("invalid denomination. please enter positive amount");
+			
+		} else if(amount> balance) {
+			
+			System.out.println("Insufficient Funds");
+			
+		}else {
+			
+			balance-=amount;
+			System.out.println("please collect your cash");
+			
+		}
+		
+	}
+
+	public void creditInterest() {
+		// TODO Auto-generated method stub
+		balance+=(balance*interestRate)/1200; //one month interest at a time.
 	}
 
 }
--- JavaProjects/in.conceptarchitect.banking/src/in/conceptarchitect/banking/app/Program.java
+++ JavaProjects/in.conceptarchitect.banking/src/in/conceptarchitect/banking/app/Program.java
@@ -1,6 +1,7 @@
 package in.conceptarchitect.banking.app;
 
 import in.conceptarchitect.banking.BankAccount;
+import in.conceptarchitect.utils.Input;
 
 public class Program {
 
@@ -11,7 +12,42 @@ public class Program {
 		
 		//a1.createAccount(1, "Vivek","p@ss",20000,12); //no need to call this line
 		
-		a1.show();
+		int choice=0;
+		Input input=new Input();
+		do {
+			
+			choice=input.readInt("1. Deposit\n2. Withdraw\n3.Credit Interst\n4.Show\n0.Exit\nChoose:");
+			
+			switch(choice) {
+			case 1:
+				a1.deposit();
+				break;
+			case 2:
+				a1.withdraw();
+				break;
+				
+			case 3:
+				a1.creditInterest();
+				break;
+				
+			case 4:
+				a1.show();
+				break;
+			case 0:
+				break;
+			default:
+				System.out.println("Invalid Input.Retry");
+				
+				
+			}
+			
+			System.out.println("\n\n");
+			
+		}while(choice!=0);
+		
+		
+		
+		
 		
 		
 		
