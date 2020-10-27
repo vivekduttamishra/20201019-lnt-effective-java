16a02846657d8cec96149b14baa85f85429fa041
Vivek Dutta Mishra <vivek@conceptarchitect.in>
2020-10-27 17:21:25 +0530

    day#6-final

--- JavaProjects/in.conceptarchitect.banking4/src/in/conceptarchitect/banking/tests/OverdraftAccountTests.java
+++ JavaProjects/in.conceptarchitect.banking4/src/in/conceptarchitect/banking/tests/OverdraftAccountTests.java
@@ -1,9 +1,56 @@
 package in.conceptarchitect.banking.tests;
 
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
 public class OverdraftAccountTests {
 
-	public OverdraftAccountTests() {
-		// TODO Auto-generated constructor stub
+
+	@Test
+	public void account_hasOdLimitAs10PercentOfInitialDeposit() {
+		fail("Implement the logic here");
+	}
+	
+	@Test
+	public void odLimit_increasesOdLimitIfBalanceIncreasesToHistoricHightestValue() {
+		fail("Implement the logic here");
+	}
+	
+	@Test
+	public void odLimit_doesntChangeOnWithdrawal() {
+		fail("Implement the logic here");
+	}
+	
+	@Test
+	public void odLimit_canIncreaseIfCreditInterestIncreasesAccountBalanceUptoHistoricMax() {
+		fail("Implement the logic here");
+	}
+	
+	@Test
+	public void odLimit_doesntChangeIfNewBalanceIsLessThanHistoricMaxBalance() {
+		fail("Implement the logic here");
+	}
+	
+		
+	@Test
+	public void withdraw_canWithdrawUptoBalancePlusOdLimit() {
+		fail("Implement the logic here");
 	}
 
+	@Test
+	public void withdraw_canPushMyBalanceToNegative() {
+		fail("Implement the logic here");
+	}
+	
+	@Test
+	public void withdraw_overlimitAttracts1PCOdFee() {
+		fail("Implement the logic here");
+	}
+	
+	
+	
+	
+	
+	
 }
