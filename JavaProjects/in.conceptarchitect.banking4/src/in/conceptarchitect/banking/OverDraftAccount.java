072e2e92390c8d3de0a0b2b0a9f730ce67317371
Vivek Dutta Mishra <vivek@conceptarchitect.in>
2020-10-27 17:20:40 +0530

    day6-update5

---
+++ JavaProjects/in.conceptarchitect.banking4/src/in/conceptarchitect/banking/OverDraftAccount.java
@@ -0,0 +1,10 @@
+package in.conceptarchitect.banking;
+
+public class OverDraftAccount extends BankAccount {
+
+	public OverDraftAccount(String name, String password, double amount) {
+		super(name, password, amount);
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ JavaProjects/in.conceptarchitect.banking4/src/in/conceptarchitect/banking/tests/OverdraftAccountTests.java
@@ -0,0 +1,9 @@
+package in.conceptarchitect.banking.tests;
+
+public class OverdraftAccountTests {
+
+	public OverdraftAccountTests() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
