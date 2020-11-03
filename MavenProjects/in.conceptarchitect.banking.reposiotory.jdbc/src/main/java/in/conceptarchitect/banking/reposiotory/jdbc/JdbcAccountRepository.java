c7fa5dae73107db4e48bdea8339ba3ab05157aa6
Vivek Dutta Mishra <vivek@conceptarchitect.in>
2020-11-03 15:20:05 +0530

    day#12-update#4

--- MavenProjects/in.conceptarchitect.banking.reposiotory.jdbc/src/main/java/in/conceptarchitect/banking/reposiotory/jdbc/JdbcAccountRepository.java
+++ MavenProjects/in.conceptarchitect.banking.reposiotory.jdbc/src/main/java/in/conceptarchitect/banking/reposiotory/jdbc/JdbcAccountRepository.java
@@ -1,19 +1,106 @@
 package in.conceptarchitect.banking.reposiotory.jdbc;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.Collection;
 
 import in.conceptarchitect.banking.core.BankAccount;
+import in.conceptarchitect.banking.core.CurrentAccount;
+import in.conceptarchitect.banking.core.OverDraftAccount;
+import in.conceptarchitect.banking.core.SavingsAccount;
 import in.conceptarchitect.banking.repository.AccountRepository;
 
 public class JdbcAccountRepository implements AccountRepository {
+	
+	String url;
+	String userName="root";
+	String password="";
 
-	public JdbcAccountRepository() {
+	public JdbcAccountRepository(String url) {
 		// TODO Auto-generated constructor stub
+		this.url=url;
 	}
+	
+	
+
+	public String getUrl() {
+		return url;
+	}
+
+
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+
+
+	public String getUserName() {
+		return userName;
+	}
+
+
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+
+
+	public String getPassword() {
+		return password;
+	}
+
+
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+
 
 	@Override
 	public int addAccount(BankAccount account) {
 		// TODO Auto-generated method stub
+		
+		Connection con=null;
+		try {
+			con=DriverManager.getConnection(url,userName,password);
+			Statement statement=con.createStatement();
+			
+			double odLimit=0;
+			if(account instanceof OverDraftAccount) {
+				OverDraftAccount od=(OverDraftAccount) account;
+				odLimit=od.getOdLimit();
+			}
+			
+			String qry=String.format("insert into bankaccounts(account_type,name,password,balance,odLimit) "
+									+ "values('%s','%s','%s',%f,%f)", 
+									  account.getClass().getName(),
+									  account.getName(),
+									  account.getEncryptedPassword(),
+									  account.getBalance(),
+									  odLimit
+									);
+			
+			statement.executeUpdate(qry);
+			
+			return 0;
+			
+		} catch(SQLException ex) {
+			ex.printStackTrace();
+		} finally {
+			if(con!=null) {
+				try {con.close();}catch(Exception ex) {}
+			}
+		}
+		
+		
+		
 		return 0;
 	}
 
@@ -32,7 +119,56 @@ public class JdbcAccountRepository implements AccountRepository {
 	@Override
 	public Collection<BankAccount> getAllAccounts() {
 		// TODO Auto-generated method stub
-		return null;
+		Connection con=null;
+		try {
+			con=DriverManager.getConnection(url,userName,password);
+			Statement statement=con.createStatement();			
+			ResultSet rs=statement.executeQuery("select * from bankaccounts");
+			
+			Collection<BankAccount> accounts=new ArrayList<BankAccount>();
+			while(rs.next()) {
+			
+				BankAccount account=null;
+				String accountType=rs.getString("account_type");
+				int accountNumber=rs.getInt("account_number");
+				String name=rs.getString("name");
+				String password=rs.getString("password");
+				double balance= rs.getDouble("balance");
+				double odLimit= rs.getDouble("odLimit");
+						
+				switch(rs.getString("account_type")) {
+					default:case "in.conceptarchitect.banking.core.SavingsAccount":
+						account=new SavingsAccount(name,"", balance); break;
+					case "in.conceptarchitect.banking.core.CurrentAccount":
+						account=new CurrentAccount(name,"", balance); break;
+					case "in.conceptarchitect.banking.core.OverDraftAccount":
+						account=new OverDraftAccount(name,"", balance); break;
+				}
+				account.setAccountNumber(accountNumber);
+				account.setInternalPassword(password);
+				if(account instanceof OverDraftAccount)
+					((OverDraftAccount) account).setOdLimit(odLimit);
+				
+				accounts.add(account);
+				
+			}
+			
+			return accounts;
+			
+		}catch(SQLException ex) {
+			ex.printStackTrace();
+			throw new RuntimeException(ex.getMessage(), ex);
+		}finally {
+			if(con!=null) {
+				try {
+					con.close();
+				}catch(Exception ex) {
+					
+				}
+			}
+		}
+		
+		
 	}
 
 	@Override
